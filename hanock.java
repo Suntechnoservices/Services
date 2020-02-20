7fb14c699295c409f6c2f20a85c0e919d376c05e
thathuri hanock kumar <hanock@suntechnoservices.comgit config --global user.email hanock@suntechnoservices.comgit config --global user.email hanock@suntechnoservices.com>
2020-02-20 13:11:09 +0530

    hello

---
+++ hanock.java
@@ -0,0 +1,18 @@
+package vidikas;
+
+public class hanock {
+	int c,a=12,b=89;
+	void method()
+	{
+		System.out.println(" sum of two no is");
+		c=a+b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hanock a=new hanock();
+		a.method();
+	}
+
+}
